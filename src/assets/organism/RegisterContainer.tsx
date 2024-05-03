d2bf16a9306461d6021d9f67d9db8ea38f8056b1
Dinuk1994 <ddt94119@gmail.com>
2024-05-03 14:31:30 +0530

    bug fixed in register page

--- src/assets/organism/RegisterContainer.tsx
+++ src/assets/organism/RegisterContainer.tsx
@@ -56,27 +56,6 @@ const RegisterContainer = () => {
     const password = watch("password")
 
 
-    const loginSuccess = () => {
-        Command: toastr["success"]("Inconceivable!")
-
-        toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
-    }
-
     return (
         <div>
             <form action="" noValidate>
