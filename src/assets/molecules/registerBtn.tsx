b3b062f21d836fc5be63e8d9ccb82af26c5e54f8
Dinuk1994 <ddt94119@gmail.com>
2024-05-07 22:26:13 +0530

    adding update button to admin page

--- src/assets/molecules/registerBtn.tsx
+++ src/assets/molecules/registerBtn.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 interface registerBtnProps{
     name : string
     click? : ()=> void;
+    makeDissable? : boolean;
 }
 
-const registerBtn: React.FC<registerBtnProps> = ({name,click}) => {
+const registerBtn: React.FC<registerBtnProps> = ({name,click,makeDissable}) => {
   return (
     <div >
-        <button onClick={click}  className='rounded-3xl shadow-2xl w-40  text-xl border-4 border-gray-300 font-serif text-white bg-btn-color p-1 hover:bg-emerald-600'>{name}</button>
+        <button onClick={click} disabled={makeDissable} className='rounded-3xl shadow-2xl w-40  text-xl border-4 border-gray-300 font-serif text-white bg-btn-color p-1 hover:bg-emerald-600'>{name}</button>
     </div>
   )
 }
--- src/assets/organism/AdminContainer.tsx
+++ src/assets/organism/AdminContainer.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FooterElement from '../molecules/Footer'
 import { useForm } from "react-hook-form";
 import RegBtn from '../molecules/registerBtn'
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import SampleImage from '../../../public/Login/Login.jpg'
 
 const AdminContainer = () => {
 
     const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
-
+    const [isChecked, setIsCkecked] = useState(false);
+    const [itemDetails, setItemDeails] = useState<any>({});
+    const [id, setId] = useState<number>();
 
     const handleFileChange = (event: any) => {
         if (event.target.files.length > 0) {
@@ -23,10 +26,12 @@ const AdminContainer = () => {
     const {
         register,
         handleSubmit,
+        setValue,
         watch,
         formState: { errors }
     } = useForm({
         defaultValues: {
+            id: '',
             category: 'men',
             name: "",
             code: "",
@@ -36,7 +41,6 @@ const AdminContainer = () => {
             qtyInStock: ""
         }
     })
-
     const submitForm = (data: any) => {
         const formData = new FormData();
         Object.keys(data).forEach((key) => {
@@ -49,21 +53,18 @@ const AdminContainer = () => {
             .then(response => {
                 console.log("Response data : ", response.data);
                 if (response.data === "product added") {
-                    alert("Product addded successfully")
                     Swal.fire({
-                        position : "center",
-                        icon : "success",
-                        title : "Product added",
-                        timer : 1500
-
+                        position: "center",
+                        icon: "success",
+                        title: "Product added",
+                        timer: 1500
                     })
                 } else {
                     Swal.fire({
-                        position : "center",
-                        icon : "error",
-                        title : "Product added failed",
-                        timer : 1500
-
+                        position: "center",
+                        icon: "error",
+                        title: "Product added failed",
+                        timer: 1500
                     })
                 }
             })
@@ -71,9 +72,74 @@ const AdminContainer = () => {
                 console.log(err);
 
             });
-
-
     }
+    const updateItem = (data:any) => {
+        const formData = new FormData();
+        Object.keys(data).forEach((key) => {
+            formData.append(key, data[key]);
+        });
+        if (selectedFile) {
+            formData.append('image', selectedFile);
+        }
+        axios.post("http://localhost:8081/product/post", formData)
+            .then(response => {
+                console.log("Response data : ", response.data);
+                if (response.data === "product added") {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Product added",
+                        timer: 1500
+                    })
+                } else {
+                    Swal.fire({
+                        position: "center",
+                        icon: "error",
+                        title: "Product added failed",
+                        timer: 1500
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err);
+
+            });
+    }
+
+    const getItem = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8081/product/getById/${id}`);
+            if (response.data) {
+                setItemDeails(response.data);
+                console.log(response.data.image);
+                
+                setValue('category', response.data.category);
+                setValue('name', response.data.name);
+                setValue('code', response.data.code);
+                setValue('description', response.data.description);
+                setValue('price', response.data.price);
+                setValue('image', response.data.image);
+                setValue('qtyInStock', response.data.qtyInStock);
+            } else {
+                setItemDeails({});
+                setValue('category', 'men'); 
+                setValue('name', '');
+                setValue('code', '');
+                setValue('description', '');
+                setValue('price', '');
+                setValue('image', '');
+                setValue('qtyInStock', '');
+            }
+        } catch (error) {
+            console.error('Error fetching item details:', error);
+        }
+    };
+
+    useEffect(() => {
+        if (isChecked && id) {
+            getItem();
+        }
+    }, [isChecked, id]);
 
 
     return (
@@ -93,7 +159,6 @@ const AdminContainer = () => {
                                         }
                                     })}
                                 />
-
                             </div>
                             <div className='flex flex-col'>
                                 <label htmlFor="women" className='text-xl font-serif font-bold ml-40'>Women</label>
@@ -129,7 +194,7 @@ const AdminContainer = () => {
                             />
                             <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.name?.message}</p>
                             <label htmlFor="" className='ml-24 mt-1 text-xl font-serif font-bold '>Code</label>
-                            <input type="text" className='rounded-lg bg-gray-200 ml-14 text-black'
+                            <input type="text" id='code' className='rounded-lg bg-gray-200 ml-14 text-black'
                                 {...register("code", {
                                     required: {
                                         value: true,
@@ -137,8 +202,9 @@ const AdminContainer = () => {
                                     }
                                 })}
                             />
-                             <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.code?.message}</p>
+                            <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.code?.message}</p>
                         </div>
+
                         <div className='flex mt-8'>
                             <div className='mt-4'><label htmlFor="" className='ml-10 text-xl font-serif font-bold '>Description</label></div>
                             <textarea className="rounded-lg bg-gray-200 ml-14 w-96 text-black"
@@ -149,8 +215,9 @@ const AdminContainer = () => {
                                     }
                                 })}
                             />
-                             <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.description?.message}</p>
+                            <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.description?.message}</p>
                         </div>
+                        <img id='image' src={selectedFile ? URL.createObjectURL(selectedFile) : itemDetails.image} alt="" className='w-44 absolute ml-[690px] mt-[210px] rounded-lg border-2 border-white shadow-lg shadow-black' />
                         <div className='flex mt-8'>
                             <label htmlFor="" className='ml-10 mt-1 text-xl font-serif font-bold '>Price</label>
                             <input type="number" className='rounded-lg bg-gray-200 ml-[124px] text-black'
@@ -161,19 +228,21 @@ const AdminContainer = () => {
                                     }
                                 })}
                             />
-                             <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.code?.message}</p>
+                            <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.code?.message}</p>
                         </div>
                         <div className='flex mt-8'>
                             <label htmlFor="" className='ml-10 mt-1 text-xl font-serif font-bold '>Image</label>
                             <input type="file" accept="image/*" className='rounded-lg bg-gray-200 ml-[115px] text-black'
-                                {...register("image",{required:{
-                                    value : true,
-                                    message : "Image is required!"
-                                }})} onChange={handleFileChange}
+                                {...register("image", {
+                                    required: {
+                                        value: true,
+                                        message: "Image is required!"
+                                    }
+                                })} onChange={handleFileChange}
                             />
-                             <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.image?.message}</p>
+                            <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.image?.message}</p>
                         </div>
-                        <div className='flex mt-8 mb-8'>
+                        <div className='flex mt-8'>
                             <label htmlFor="" className='ml-10 mt-1 text-xl font-serif font-bold '>Qty in stock</label>
                             <input type="number" className='rounded-lg bg-gray-200 ml-[60px] text-black w-32'
                                 {...register("qtyInStock", {
@@ -183,11 +252,42 @@ const AdminContainer = () => {
                                     }
                                 })}
                             />
-                             <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.qtyInStock?.message}</p>
+                            <p className='text-red-900 font-serif text-xs flex justify-end mt-1'>{errors.qtyInStock?.message}</p>
                         </div>
-                        <div className='flex justify-center mb-10 '>
-                            <RegBtn name='Add Item' click={handleSubmit(submitForm)} />
+                        <div>
+                            <div className='flex mt-8 mb-8'>
+                                <label htmlFor="" className='ml-10 mt-1 text-xl font-serif font-bold '>Item ID</label>
+                                <input
+                                    type="number"
+                                    value={id||''}
+                                    className='rounded-lg bg-gray-200 ml-[100px] text-black w-40'
+                                    disabled={!isChecked}
+                                    {...register("id")}
+                                    onChange={(e) => {
+                                        setId(Number(e.target.value));
+                                        getItem();
+                                    }}
+                                />
+                                <input
+                                    type="checkbox"
+                                    className='ml-4 mt-3'
+                                    checked={isChecked}
+                                    onChange={() => setIsCkecked(!isChecked)}
+                                />
+                                <label className='mt-2 ml-2 text-red-700' htmlFor="">If you want to UPDATE the item please enter the item ID number*</label>
+                            </div>
                         </div>
+                        <div className='flex grid-cols-2 justify-center mb-10 '>
+                            <div className='grid-cols-1'>
+
+                            </div>
+                            <div className='flex justify-between '>
+
+                                <div> <RegBtn name='Update item' makeDissable={!isChecked} click={handleSubmit(updateItem)} /></div>
+                                <div className='ml-10' ><RegBtn name='Add Item' makeDissable={isChecked} click={handleSubmit(submitForm)} /></div>
+                            </div>
+                        </div>
+
 
                     </div>
                 </div>
